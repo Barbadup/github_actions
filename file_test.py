31783844a20980bc19378461e79a6110624930ae
Damien <damien.dupont@orange.com>
2025-01-20 10:46:43 +0000

    transformation 2 en PEP8

--- file_test.py
+++ file_test.py
@@ -1,22 +1,22 @@
-import pytest
-
 # Fonction test du résultat de 2 + 4
 def test_calc_addition():
     output = 1 + 4
     assert output == 5 
+
 
 # Fonction test du résultat de 2 - 4
 def test_calc_subtraction():
     output = 2 - 4
     assert output == -2
 
+
 # Fonction test du résultat de 2 * 4
 def test_calc_multiply():
     output = 2 * 4
     assert output == 8
 
+
 # Fonction test si la résultat renvoie 'hello'
 def test_coucou():
     output = 'hello'
     assert output == 'hello'
-
